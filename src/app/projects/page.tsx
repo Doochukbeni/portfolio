1c9f4f454ca95808ce26969efe05854e264f237b
Doochukbeni <doochukbeni@gmail.com>
2023-09-22 14:25:32 +0400

    remove proejct layout page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,6 +3,9 @@ import HomeProject from "@/components/(projects)/HomeProject";
 const page = () => {
   return (
     <div className="min-h-full h-screen">
+      <h1 className="text-3xl lg:text-5xl text-slate-700 my-10 dark:text-slate-300 font-bold text-center">
+        These are some of my portfolio projects
+      </h1>
       <HomeProject />
     </div>
   );
